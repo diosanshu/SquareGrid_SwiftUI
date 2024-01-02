6eb81a4e3359cd11809f5aee85345bb8438c829b
diosanshu <diosanshu@gmail.com>
2024-01-02 15:03:43 +0530

    IDE updated

---
+++ SquareGrid-SwiftUI/SquareGrid-SwiftUI/SquareGrid_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SquareGrid_SwiftUIApp.swift
+//  SquareGrid-SwiftUI
+//
+//  Created by Haadhya on 02/01/24.
+//
+
+import SwiftUI
+
+@main
+struct SquareGrid_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
